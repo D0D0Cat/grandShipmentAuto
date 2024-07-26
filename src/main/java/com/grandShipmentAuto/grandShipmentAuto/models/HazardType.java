aca0f24e644ce9ba9eccb5b4322e7a545ddc7211
D0D0Cat <tomaskatalinas@gmail.com>
2024-07-26 21:30:22 +0100

    SUPER DID THEN UPDATE3

--- src/main/java/com/grandShipmentAuto/grandShipmentAuto/models/HazardType.java
+++ src/main/java/com/grandShipmentAuto/grandShipmentAuto/models/HazardType.java
@@ -14,7 +14,7 @@ public class HazardType {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "id ")
     private long id;
 
     @Column(name = "type ")
